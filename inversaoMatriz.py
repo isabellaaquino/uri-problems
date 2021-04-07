fcfed3d48f881f28164aaadc5ae73b0f4dff900b
Isabella Aquino <76221367+isabellaaquino@users.noreply.github.com>
2021-04-07 10:38:32 -0300

    Update inversaoMatriz.py

--- inversaoMatriz.py
+++ inversaoMatriz.py
@@ -1,4 +1,4 @@
-matriz = input("Digite uma lista composta (matriz")
+matriz = input("Digite uma lista composta (matriz)")
 matrizFinal = []
 matrizFinal = [[matriz[j][i] for j in range(len(matriz))] for i in range(len(matriz[0]))]
 print(matrizFinal)
